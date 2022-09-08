7981e9f0da50fa51d4a5f1f62ad020a91e245dcf
Tana Glassford-Samuel <105245606+tags59@users.noreply.github.com>
2022-09-08 13:32:14 -0400

    Update ec2resource.tf

--- ec2resource.tf
+++ ec2resource.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "myec2" {
-  ami = "ami-09e67e426f25ce0d7"
-  instance_type = "t2.micro"
+  ami = "ami-09e67e426f25cku98jvhfytcgvhe0d7"
+  instance_type = "t.micro"
   tags = {
     name = "instance10"
   }
